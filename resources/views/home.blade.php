e79767406cce543936efb5cbeb07ba34fdc53892
Luigi <Lmname999@gmail.com>
2024-05-29 08:40:02 +0100

    Mudança nos filmes caso não exista cartaz para o filme

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,29 @@
 @extends('layouts.main')
 
-@section('header-title', 'Introduction')
+@section('header-title', 'Home Page')
 
 @section('main')
     <main>
+        <div class="max-w-7xl mt-10 mx-auto px-4">
+            <img src="{{ Vite::asset('resources/img/photos/carousel1.png') }}" alt="..." class="mx-auto w-full rounded-lg shadow-lg">
 
+            <h1 class="text-4xl text-center mt-5 font-extrabold text-gray-800">Em cartaz</h1>
 
-        <div class="max-w-7xl mt-10 mx-auto">
-            <img src="{{ Vite::asset('resources/img/photos/carousel1.png') }}" alt="..." class="mx-auto w-full ">
-
-            <h1 class="text-4xl text-center mt-5">Em cartaz</h1>
-
-            <div class="flex flex-row flex-wrap justify-evenly my-">
+            <div class="flex flex-wrap justify-center mt-8 gap-8">
                 @foreach ($movies->take(8) as $movie)
-                    <div class="relative overflow-hidden rounded-lg mt-8 max-w-72 mx-3 sm-mx-5">
-                        <img src="{{ Vite::asset('storage/app/public/posters/' . $movie->poster_filename) }}" alt="..."
-                            class="w-full">
+                    <div class="relative overflow-hidden rounded-lg shadow-lg transform transition duration-500 hover:scale-105 max-w-sm">
+                        @php
+                            $posterPath = $movie->poster_filename ? 'storage/app/public/posters/' . $movie->poster_filename : 'storage/app/public/posters/_no_poster_1.png';
+                        @endphp
+                        <img src="{{ Vite::asset($posterPath) }}" alt="..." class="w-full h-96 object-cover">
                         <div class="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-                        <h1 class="absolute bottom-10 left-5 text-3xl font-bold text-white">{{ $movie->title }}</h1>
-                        <div class="absolute bottom-5 left-5 overflow-hidden text-xs leading-4 text-gray-300">{{ $movie->genre_code }}</div>
+                        <div class="absolute bottom-0 left-0 p-5">
+                            <h1 class="text-2xl font-bold text-white">{{ $movie->title }}</h1>
+                            <p class="mt-2 text-sm text-gray-300">{{ $movie->genre_code }}</p>
+                        </div>
                     </div>
                 @endforeach
             </div>
         </div>
-
     </main>
 @endsection
